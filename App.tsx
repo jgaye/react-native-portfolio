2ad16fb0f410f567afef42082932c49719873188
Julien Gaye <julien@getluko.com>
2024-03-01 20:51:29 -0500

    Added imports for Amplify

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import 'react-native-gesture-handler';
 
 import { ThemeProvider } from '@shopify/restyle';
+import { Amplify } from 'aws-amplify';
 import { theme } from 'theme';
 
+import amplifyconfig from './src/amplifyconfiguration.json';
 import RootStack from './src/navigation';
+Amplify.configure(amplifyconfig);
 
 export default function App() {
   return (
